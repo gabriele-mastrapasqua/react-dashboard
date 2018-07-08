9135c9a8dcf3c046cbd0f96cdae618ceff9a5385
Gabriele Mastrapasqua <gabriele@presstoday.com>
2018-07-08 22:55:42 +0200

    using validation and geospatial index generation on event location point

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
- 
+var GeoJSON = require('mongoose-geojson-schema');
+
 var eventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     device_id: Number,
     // geojson format: Mongoose interpets this as 'location is an object with 2 keys, type and coordinates'
-    location:{
+    /*location:{
         type: {type: String, default: 'Point'},
         coordinates: {type: [Number], default: [0, 0]}
-    },
+    },*/
+    location: mongoose.Schema.Types.Point,
     timestamp: Date
 });
  
