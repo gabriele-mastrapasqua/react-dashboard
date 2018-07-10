fe317781d613f0cd3bbdb185bd05b5b08f61430b
Gabriele Mastrapasqua <gabriele@presstoday.com>
2018-07-10 20:27:26 +0200

    removed material table

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios'
-
-
-/*import './BarChart';
-import BarChart from './BarChart';
-import WorldMap from './WorldMap';
-*/
 import config from './config.js';
 
-
-import EnhancedTable from './EnhancedTable';
-
+// ui components
+import DevicesImpressionsTable from './DevicesImpressionsTable';
 
 const ReactHighmaps = require('react-highcharts/ReactHighmaps');
 import maps from './map';
@@ -38,7 +31,7 @@ const mapconfig = {
         enabled: true,
         color: 'white',
         formatter: function () {
-            return this.point.properties['level1'];          
+          return this.point.properties['level1'];
         },
         format: null,
         style: {
@@ -57,27 +50,27 @@ const mapconfig = {
   // todo -> map with states from the server: 
   // do an axios req that map a json with state and count of impressions
   series: [
-    
-  {
-    name: 'Alaska',
-    data: ['Alaska'].map(function (code) {
-      return {code: code, count: 100};
-    })
-  }, 
-  {
-    name: 'Alabama',
-    data: ['Alabama' ].map(function (code) {
-      return  {code: code, count: 22};
-    })
-  },
-  {
-    name: 'Arizona',
-    data: ['Arizona'].map(function (code) {
-      return  {code: code, count: 1100};
-    })
-  },
 
-]
+    {
+      name: 'Alaska',
+      data: ['Alaska'].map(function (code) {
+        return { code: code, count: 100 };
+      })
+    },
+    {
+      name: 'Alabama',
+      data: ['Alabama'].map(function (code) {
+        return { code: code, count: 22 };
+      })
+    },
+    {
+      name: 'Arizona',
+      data: ['Arizona'].map(function (code) {
+        return { code: code, count: 1100 };
+      })
+    },
+
+  ]
 };
 
 
@@ -85,49 +78,64 @@ const mapconfig = {
 
 class App extends Component {
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = {
-        totalImpressions: 0,
-        impressionPerDevices: [],
-        impression24Hours: [],
-        impressionDayOfWeek: [],
-        impressionDayOfMonth: [],
-        impressionEachCountry: [],
-        
-      };
-      
+    this.state = {
+      totalImpressions: 0,
+      impressionPerDevices: [],
+      impression24Hours: [],
+      impressionDayOfWeek: [],
+      impressionDayOfMonth: [],
+      impressionEachCountry: [],
+
+    };
+
   }
 
   componentDidMount() {
     axios.get(config.API_URL + 'getImpressions')
-    .then(response => {
-      console.log(response)
-      this.setState({totalImpressions: response.data.impressions, impressionPerDevices: response.data.impressionPerDevices})
-    })
+      .then(response => {
+        console.log(response)
+        this.setState({ totalImpressions: response.data.impressions, impressionPerDevices: response.data.impressionPerDevices })
+      })
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Analityics Dashboard</h2>
+          <h2>Analityics Dashboard</h2>
         </div>
-        <p className="App-intro">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <b>Total impressions:</b> {this.state.totalImpressions}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="btn-group" role="group" aria-label="Basic example">
+                <button type="button" className="btn btn-secondary">Last 24 hours</button>
+                <button type="button" className="btn btn-secondary">Last week</button>
+                <button type="button" className="btn btn-secondary">Last Month</button>
+              </div>
+            </div>
+          </div>
 
-        <b>Total impressions:</b> {this.state.totalImpressions}
-
-        <EnhancedTable data={this.state.impressionPerDevices}  />
-
-      <br/>
-     
-      Impressions for each country:
-
-      <ReactHighmaps config={mapconfig}/>
+        </div>
 
 
-        </p>
+
+        <DevicesImpressionsTable data={this.state.impressionPerDevices} />
+
+        <br />
+
+        Impressions for each country:
+
+      <ReactHighmaps config={mapconfig} />
+
+
+
       </div>
     );
   }
