928afcccb0b3ab71ffc7759cc512b46a8bdc51e3
Gabriele Mastrapasqua <gabriele@presstoday.com>
2018-07-11 21:58:40 +0200

    speedup inserting time of event data

--- server/src/scripts/import-csv.js
+++ server/src/scripts/import-csv.js
@@ -10,32 +10,37 @@ const connStr = `mongodb://${MONGO_HOST}:${MONGO_PORT}/analytics`;
 mongoose.connect(connStr, { useNewUrlParser: true });
 
 // clear all previous data...
-Event.remove({}, function(error) {
+Event.remove({}, function (error) {
     console.log('there are events to remove', error);
     loadMobileData();
 });
 
 function loadMobileData() {
     var count = 0;
+    var events = [];
     console.log("start import...");
     csv.fromPath(DATA_FILENAME, {
-            headers: true,
-            ignoreEmpty: true
-        })
+        headers: true,
+        ignoreEmpty: true
+    })
         .on("data", function (data) {
             data['_id'] = new mongoose.Types.ObjectId();
             data['location'] = {
-                type: "Point", 
+                type: "Point",
                 coordinates: [parseFloat(data["lng"]), parseFloat(data["lat"])]   // takes longitude, latitude coordinates
             };
-            console.log("got data ", data);
-            Event.create([data], function (err, documents) {
-                if (err) throw err;
-            });
+            //console.log("got data ", data);
+            events.push(data);
+            console.log("insert row ", count);
             count++;
         })
         .on("end", function () {
-            console.log(count + ' have been successfully imported.');
-            return;
+            // much faster than Model.create()
+            Event.collection.insert(events, function (err, documents) {
+                if (err) throw err;
+                console.log(count + ' have been successfully imported.');
+            });
+            
+           
         });
 }
