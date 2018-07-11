4fe527ac2757320c06324edf8ce20d1ff47745b1
Gabriele Mastrapasqua <gabriele@presstoday.com>
2018-07-12 01:29:13 +0200

    add pagination

--- client/src/DevicesImpressionsTable.js
+++ client/src/DevicesImpressionsTable.js
@@ -3,33 +3,63 @@ import axios from 'axios'
 import config from './config.js';
 
 class DeviceImpressionsTable extends React.Component {
+  page = 1;
+
   constructor(props) {
     super(props);
     this.state = {
+      page: 1,
       impressionPerDevices: []
     };
+
+    // events binding
+    this.onNextClick = this.onNextClick.bind(this);
+    this.onPreviousClick = this.onPreviousClick.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
+    this.loadData()
+  }
+
+  loadData() {
     console.log("call getImpressions ");
-    axios.get(config.API_URL + 'getImpressions')
+    axios.get(config.API_URL + 'getImpressions/' + this.page)
       .then(response => {
         console.log(response)
         this.setState({ impressionPerDevices: response.data.impressionPerDevices })
       })
   }
 
+  onPreviousClick(event) {
+    if (this.state.page > 0) {
+      this.page = this.page - 1;
+      this.setState({page: this.page});
+      console.log("prev click", this.state.page)
+      this.loadData()
+    }
+  }
+
+  onNextClick(event) {
+    if (this.state.impressionPerDevices && this.state.impressionPerDevices.length > 0) {
+      this.page = this.state.page + 1;
+      this.setState({page: this.page});
+      console.log("next click", this.state.page)
+      this.loadData()
+    }
+  }
+
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Device id</th>
-            <th scope="col">Impressions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.impressionPerDevices.map((n, index) => {
+      <div>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">Device id</th>
+              <th scope="col">Impressions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.impressionPerDevices.map((n, index) => {
               return (
                 <tr key={n._id} index={index}>
                   <td>{n._id}</td>
@@ -37,8 +67,19 @@ class DeviceImpressionsTable extends React.Component {
                 </tr>
               );
             })}
-        </tbody>
-      </table>
+          </tbody>
+          <tfoot>
+            <nav aria-label="Page navigation example" >
+              <ul className="pagination justify-content-end" >
+                <li className="page-item" className={'page-item ' + (this.state.page == 1 ? 'disabled' : '')}><a className="page-link" onClick={this.onPreviousClick} >Previous</a></li>
+                <li className="page-item"><a className="page-link" onClick={this.onNextClick}>Next</a></li>
+              </ul>
+            </nav>
+          </tfoot>
+        </table>
+
+
+      </div>
     );
   }
 }
